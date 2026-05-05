f35e52cf6a09305f68df7adbac2049faf5dd5005
ahmnbyl74-alt <ahmnbyl74@gmail.com>
2026-05-05 06:22:24 +0300

    Update site name and Telegram user link

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from http import cookies
 # --- [ 1. الإعدادات والبيانات الأساسية ] ---
 PORT = int(os.environ.get("PORT", 8080))
 DB_FILE = "spider_master_database.json"
-SITE_NAME = "Spider Store Pro"
-TELEGRAM_USER = "iQSpider" # استبدله بيوزرك هنا
+SITE_NAME = "Spider Store"
+TELEGRAM_USER = "https://t.me/SmmSpider" # استبدله بيوزرك هنا
 
 # تصفير قاعدة البيانات عند التشغيل لضمان تطبيق التحديثات البرمجية وكلمة السر 123
 if os.path.exists(DB_FILE):
